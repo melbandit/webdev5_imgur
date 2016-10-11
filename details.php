2c4e1221245e89fc2dd6f891676d63d8c95547eb
melbandit <melissa.i.sattler@gmail.com>
2016-10-11 18:34:37 -0400

    fixed dropdown

--- details.php
+++ details.php
@@ -143,7 +143,11 @@
 </div>
 
 <footer class="footer">&copy; Copyright text with dynamic date</footer>
-
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
+<script src="assets/js/script.js" type="text/javascript"></script>
 
 </body>
 </html>
--- search.php
+++ search.php
@@ -236,10 +236,10 @@
 
 <footer class="footer">&copy; Copyright text with dynamic date</footer>
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
-<script src="assets/js/script.js" type="text/javascript"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
+    <script src="assets/js/script.js" type="text/javascript"></script>
 </body> <!-- END of .page and body -->
 </html>
