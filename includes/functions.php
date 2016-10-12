5e4f334c69dcd5801f8cbf62209eb62865ce690b
melbandit <melissa.i.sattler@gmail.com>
2016-10-11 20:25:50 -0400

    starting php

---
+++ includes/functions.php
@@ -0,0 +1,20 @@
+<?php include __DIR__ . '/functions.php' ?>
+
+<?php require __DIR__ . '/functions.php' ?> <!--will kill yyour program-->
+<?php include_once __DIR__ . '/functions.php' ?>
+
+<!doctype html>
+
+<html>
+
+
+<img src="<?php echo getImageUrl($img-url); ?>"/>
+
+
+</html>
+
+<?php include __DIR__ . '/header.php' ?>
+<grid-view>
+    <thumb
+</grid-view>
+<?php include __DIR__ . '/footer.php' ?>
