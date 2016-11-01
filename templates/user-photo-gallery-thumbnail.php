cd9c5a7e340f60c3ab71469a76a7b2213a2a3f87
melbandit <melissa.i.sattler@gmail.com>
2016-11-01 12:56:35 -0400

    updating my uploads page

--- templates/user-photo-gallery-thumbnail.php
+++ templates/user-photo-gallery-thumbnail.php
@@ -1,12 +1,22 @@
+<!--<a class="photo-gallery__link" href="details.php?image=--><?php //echo $image->id; ?><!--">-->
+<!--    <img class="thumbnail" src="--><?php //echo $image->url; ?><!--" alt="--><?php //echo $image->alt ?><!--"/>-->
+<!---->
+<!--    <div class="highlight">-->
+<!--        <button class="cross">&#735; Delete</button>-->
+<!--        <p class="thumbnail--title">--><?php //echo $image->title; ?><!--</p>-->
+<!--        <p class="thumbnail--description">"--><?php //echo $image->description; ?><!--"</p>-->
+<!--        <p class="thumbnail--date-time">--><?php //include __DIR__ . '/../includes/format-date.php'; ?><!--</p>-->
+<!--        <a href="upload.php">Edit</a>-->
+<!--    </div>-->
+<!--</a>-->
 <a class="photo-gallery__link" href="details.php?image=<?php echo $image->id; ?>">
     <img class="thumbnail" src="<?php echo $image->url; ?>" alt="<?php echo $image->alt ?>"/>
-
     <div class="highlight">
-        <button class="cross">&#735; Delete</button>
         <p class="thumbnail--title"><?php echo $image->title; ?></p>
-        <p class="thumbnail--description">"<?php echo $image->description; ?>"</p>
         <p class="thumbnail--date-time"><?php include __DIR__ . '/../includes/format-date.php'; ?></p>
-        <a href="upload.php">Edit</a>
+        <p class="thumbnail--description"> "<?php echo $image->description; ?>"</p>
     </div>
-</a>
+    <button class="cross">&#735; Delete</button>
 
+    <p><a class="thumbnail--edit" href="upload.php">Edit</a></p>
+</a>
